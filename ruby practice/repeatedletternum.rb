eaec6a5da7f3e5b433d416cc4f5b3a6ba73dffef
bubblemelon <c.f.2y.93@gmail.com>
2018-01-24 16:10:56 -0800

    renamed commonlettercount.rb to lettercount.rb and created repeatedletternum.rb

---
+++ ruby practice/repeatedletternum.rb
@@ -0,0 +1,10 @@
+#
+# A method that counts the number of letters that repeat ( not its frequency )
+#
+# Takes a hash with letters as keys and its frequency as values
+#
+# Returns an integer with the number of letters that has more than 1 frequency
+
+def repeatinglettersnumber()
+
+end
